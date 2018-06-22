9b855b7a153ad2dde1a730ad7b8969bc2c6147a8
Darshana Waasala <darshanawaasala@gmail.com>
2018-06-22 18:02:06 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'app';
+  imageSource = '../assets/defaultImage.png';
+  websocket = undefined;
+  fileSelected = false;
+
+  ngOnInit(){
+    this.websocket = new WebSocket("ws://127.0.0.1:5678/");
+    this.websocket.onmessage = function (event) {
+      let data = JSON.parse(event.data);
+      console.log('data.....',data)
+    }
+  }
+
+  onChange(event){
+    let reader = new FileReader();
+    
+    const file: File = event.target.files[0];
+    
+    reader.readAsDataURL(file);
+    reader.onload = () =>{
+      this.imageSource = reader.result;
+      this.fileSelected = true;
+    }
+    
+  }
+
+  clearImage(){
+    this.fileSelected = false;
+    this.imageSource = '../assets/defaultImage.png';
+  }
+  
+  checkForClone(){
+    this.websocket.send(JSON.stringify({image: this.imageSource}));
+  }
+}
